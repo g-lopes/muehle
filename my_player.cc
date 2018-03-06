74e52d15fcf180f1883b706f66f9613bfba1b071
Guilherme Calábria Lopes <guicalabria@gmail.com>
2018-03-06 17:42:05 +0100

    Added unmodified files from the source

---
+++ my_player.cc
@@ -0,0 +1,9 @@
+int main(void)
+{
+	while (1) {
+		/* TODO: fill in your own player code here.
+		 * You may look at example_player.c for obtaining the game state from
+		 * the MCP and how to send your move back. */
+	}
+	return 0;
+}
